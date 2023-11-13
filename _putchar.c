ebf2d3babf22f9a61f2e1a65b6799a51b1fafdcf
Dre <gbemigadare@gmail.com>
2023-11-13 05:29:56 -0800

    Create _putchar func and header file.

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints given character c to stdout of 1.
+ *
+ * @c: character to be printed
+ *
+ * Return: returns 1 if successful and -1 if error's encountered
+ */
+
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
