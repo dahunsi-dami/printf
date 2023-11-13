2cd4a54ed3295385064a5109798f473a8eead7de
Dre <gbemigadare@gmail.com>
2023-11-13 08:56:28 -0800

    Check for edge case where format is empty.

--- _printf.c
+++ _printf.c
@@ -12,6 +12,11 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
+	if (format == NULL)
+	{
+		va_end(args);
+		return (1);
+	}
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] == '%')
