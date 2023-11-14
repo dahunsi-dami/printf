89f4b08a83cce66e10c41080adbf8ad4632f5e20
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-14 09:14:08 -0800

    function to print integer numbers to standard output

---
+++ _print_d.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _print_d - prints and integer to standard output
+ * @number: number to be printed
+ *
+ * Return: On success (length of string
+ * otherwise (-1))
+ */
+int _print_d(int number)
+{
+	int numCopy = number, denom = 1, result;
+	int count = 0;
+
+	while (numCopy > 10)
+	{
+		denom *= 10;
+		numCopy /= 10;
+	}
+
+	while (denom > 0)
+	{
+		result = number / denom;
+		_putchar(result + '0');
+		number %= denom;
+		denom /= 10;
+		count++;
+	}
+
+	return (count);
+}
