609a21c26c0fdb8ed590baefe2844ed985b0c63e
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-14 16:35:35 -0800

    Adjustment to betty errors

--- _print_s.c
+++ _print_s.c
@@ -8,14 +8,16 @@
 int _print_s(char *str)
 {
 	int i;
-		
+
 	if (str == NULL)
 	{
-			i = write(1, "(null)", 6);
-			return (i);
+		i = write(1, "(null)", 6);
+		return (i);
 	}
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
 	return (i);
 }
