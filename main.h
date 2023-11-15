80ee29250c1fe193f73b57578d2b03bf9b91e154
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-14 23:45:37 -0800

    _print_d argument's datatype changed to 'long int' from 'int'

--- main.h
+++ main.h
@@ -9,6 +9,6 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int _print_s(char *str);
 int _print_all(char forspec, va_list args);
-int _print_d(int number);
+int _print_d(long int number);
 
 #endif
