9e6149a3a9f984e25ffe5d10942f5b88611a6799
Dre <gbemigadare@gmail.com>
2023-11-14 17:00:45 -0800

    Unhandle unknown specifiers.

--- _print_all.c
+++ _print_all.c
@@ -35,11 +35,6 @@ int _print_all(char forspec, va_list args)
 					return (sum);
 			}
 		}
-		else
-		{	
-			sum += _putchar('%');
-			sum += _putchar(_putchar(va_arg(args, int)));
-		}
 		i++;
 	}
 	exit(-1);
