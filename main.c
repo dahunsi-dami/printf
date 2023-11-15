4fbfad0a6ea7049b5e4be34736cdb198a4cacb0d
Dre <gbemigadare@gmail.com>
2023-11-14 18:15:05 -0800

    Separate newly added edge case with parenthesis.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	_printf("The time is %c o'clock.\n", '2');
+	_printf("The time is %t o'clock.\n");
+	/* _printf("The time is % o'clock.\n"); */
+	_printf("The time is %");
+
+	return (0);
+}
