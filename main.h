27440cf412a6fe5e99451edf168a81eb75e08975
Dre <gbemigadare@gmail.com>
2023-11-17 19:21:47 -0800

    Add all new function prototypes.

--- main.h
+++ main.h
@@ -10,6 +10,10 @@ int _printf(const char *format, ...);
 int _print_s(char *str);
 int _print_all(char forspec, va_list args);
 int _print_d(long int number);
+int _pow(int base, int pow);
+int _binary(int num);
+int main(void);
 int _add_to_buffer(char *str, char *buffer);
+int _send_to_buffer(char *s);
 
 #endif
