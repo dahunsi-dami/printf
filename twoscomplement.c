0d667166e93fc554711e5be3e18b139c16704e54
Dre <gbemigadare@gmail.com>
2023-11-18 05:15:12 -0800

    Create first draft of twocomplement function to be used later for binary.

---
+++ twoscomplement.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+void twoscomplement(char *buffer)
+{
+	int i = 0, j;
+	while (buffer[i])
+		i++;
+	i -= 1;
+	while (i >= 0)
+	{
+		if (buffer[i] == '1')
+			break;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		if (buffer[j] == '0')
+			buffer[j] = 1 + '0';
+		else if (buffer[j] == '1')
+			buffer[j] = 0 + '0';
+	}
+
+	for (int i = 0; buffer[i] != '\0'; i++)
+		_printf("%d", buffer[i] - '0');
+	_printf("\n");
+}
