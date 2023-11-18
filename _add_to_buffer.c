7eccb414c88fbb789ab50757cc6cfb858946fda5
Dre <gbemigadare@gmail.com>
2023-11-18 04:18:55 -0800

    Slot chars into buffer one by one as it receives from _dtob.c function.

--- _add_to_buffer.c
+++ _add_to_buffer.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _add_to_buffer - adds a specified string to a buffer.
- * @str: the string to be added to the buffer.
+ * _add_to_buffer - adds a specified character to a buffer.
+ * @s: the character to be added to the buffer.
  * @buffer: the buffer to collect & hold the string.
  *
  * Return: returns the number of elements in the buffer.
  */
-int _add_to_buffer(char *str, char *buffer)
+int _add_to_buffer(char s, char *buffer)
 {
-	int i = 0, j = 0, k = 0;
+	int i = 0, j = 0;
 
 	while (*buffer != '\0')
 	{
@@ -17,31 +17,11 @@ int _add_to_buffer(char *str, char *buffer)
 		i++;
 	}
 
-	for (k = 0; str[k] != '\0'; k++)
+	for (j = 0; str[j] != '\0'; j++)
 	{
-		buffer[i] = str[k];
+		buffer[i] = str[j];
 		i++;
 	}
 
-	for (j = 0; j < i; j++)
-		_putchar(buffer[j]);
-	_putchar('\n');
 	return (i);
-}
-
-/**
- * _send_to_buffer  - function takes a string and adds it to buffer.
- * @s: the string to be added to the buffer.
- *
- * Return: always 0 (success).
- */
-int _send_to_buffer(char *s)
-{
-	char buffer[1024] = "";
-
-	char *test = s;
-
-	_add_to_buffer(test, buffer);
-
-	return (0);
 }
