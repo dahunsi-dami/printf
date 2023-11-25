1f33e310800443aefeb53b1d297385c37635be9f
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-25 07:48:51 -0800

    Removed _pow function prototype

--- main.h
+++ main.h
@@ -10,7 +10,6 @@ int _printf(const char *format, ...);
 int _print_s(char *str);
 int _print_all(char forspec, va_list args);
 int _print_d(long int number);
-int _pow(int base, int pow);
 int _dtob(long int num);
 int _add_to_buffer(char s, char *buffer);
 int _twoscomplement(long int b, char *buffer);
