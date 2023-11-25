ddeff5902aa161f466006819b2cbe2b320ec80ff
Dre <gbemigadare@gmail.com>
2023-11-25 05:25:10 -0800

    Adjust while loop to do decrement, and count length of buffer too.

--- _twoscomplement.c
+++ _twoscomplement.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
-void _twoscomplement(char *buffer)
+/**
+ * _twoscomplement - performs math op twos complement on bufer.
+ * @buffer: the buffer containing binary form of number.
+ *
+ * Return: the length of the buffer.
+ */
+int _twoscomplement(char *buffer)
 {
-	int i = 0, j;
+	int i = 0, j, k, len = 0;
+
 	while (buffer[i])
 		i++;
 	i -= 1;
@@ -10,13 +16,21 @@ void _twoscomplement(char *buffer)
 	{
 		if (buffer[i] == '1')
 			break;
+		i--;
 	}
 
-	for (j = i - 1; j >= 0; j--)
+	for (j = i - 1; j >= 0; j--, len++)
 	{
 		if (buffer[j] == '0')
 			buffer[j] = 1 + '0';
 		else if (buffer[j] == '1')
 			buffer[j] = 0 + '0';
 	}
+	for (k = 0; buffer[k]; k++)
+		len++;
+	/*	if (buffer[k] == '1') */
+	/*		break; */
+	/* for (; buffer[k]; k++) */
+	/*	len++; */
+	return (len);
 }
